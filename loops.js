33ada7dec535619083a4468fb36ecd0f43ec5a09
Jens Pehrson <jens@nygatan.se>
2017-08-27 21:07:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,34 @@
+function forLoop(array){
+  for (var i = 0; i < 25; i++){
+    if (i == 0){
+      array.push("I am a strange loop.")
+    }
+    else{
+      array.push(`I am ${i} strange loop.`)
+
+    }
+}
+return array
+}
+
+function whileLoop(number){
+  while (number > 0){
+    console.log(number)
+    number -=1
+  }
+  return ("done")
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+
+
+}
